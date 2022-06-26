befe4d54767047c7987d60e5ebf77adbd0287d16
lequyetanh <lequyetanh@outlook.com>
2022-06-26 16:39:43 +0700

    add .env and fix index

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const connectDB = require("./db/connectDB");
 require("dotenv").config();
-
+const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const start = async () => {
   try {
-    await connectDB(process.env.MONGo_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
